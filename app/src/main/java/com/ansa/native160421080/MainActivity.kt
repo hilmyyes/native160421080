c003daa508418266ea65fe6abcf50bfbab1df94a
hilmyyes <irfansahilmy@gmail.com>
2024-02-22 07:56:21 +0700

    init

---
+++ app/src/main/java/com/ansa/native160421080/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ansa.native160421080
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // a new comment
+        // test buat percobaan baru: we need implement login features
+    }
+}
