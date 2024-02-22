131ba4d4fcd0476bdb40272f42a516860f960150
hilmyyes <irfansahilmy@gmail.com>
2024-02-22 08:28:30 +0700

    Login method added

--- app/src/main/java/com/ansa/native160421080/Login.java
+++ app/src/main/java/com/ansa/native160421080/Login.java
@@ -2,4 +2,12 @@ package com.ansa.native160421080;
 
 class Login {
     String username; String password;
+
+    @Override
+    public String toString() {
+        return "Login{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
