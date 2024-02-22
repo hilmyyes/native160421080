0612f27ddb208b2733929072abad69bc6e5f6a9d
hilmyyes <irfansahilmy@gmail.com>
2024-02-22 07:59:31 +0700

    back pressed check added

--- app/src/main/java/com/ansa/native160421080/MainActivity.kt
+++ app/src/main/java/com/ansa/native160421080/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ansa.native160421080
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.window.OnBackInvokedDispatcher
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +10,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // a new comment
         // test buat percobaan baru: we need implement login features
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
